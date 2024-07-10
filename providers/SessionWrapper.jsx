5be8b25ca0cebac245d17cb9da893fa931579dc9
sohseyedi <sohseyedi@gmail.com>
2024-07-10 11:20:00 +0330

    session wrapper

---
+++ providers/SessionWrapper.jsx
@@ -0,0 +1,7 @@
+import { SessionProvider } from "next-auth/react";
+
+const SessionWrapper = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default SessionWrapper;
