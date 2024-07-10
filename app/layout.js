23d039ca7a05fcf434f69f1a4f35faa14047ab71
sohseyedi <sohseyedi@gmail.com>
2024-07-10 10:24:02 +0330

    change meta data

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
+  title: 'KifLink',
+  description: 'One Link - One World',
 }
 
 export default function RootLayout({ children }) {
