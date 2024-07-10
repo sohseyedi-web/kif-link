a70828cb641a345d29c9acead8a853368d878923
sohseyedi <sohseyedi@gmail.com>
2024-07-10 11:11:29 +0330

    config next-auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth/next";
+
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
+
+export { handler };
