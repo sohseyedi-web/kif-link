c481204ce4c3b6a0fefd0e5fe433953b34e0cf58
sohseyedi <sohseyedi@gmail.com>
2024-07-23 09:41:34 +0330

    pallet box start

--- components/dashboard/account/PalletBox.jsx
+++ components/dashboard/account/PalletBox.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 const PalletBox = () => {
   return (
     <div>
       <label className="block mb-3 lg:text-lg">انتخاب پالت :</label>
-      <div className="w-full p-3 my-3 h-[70px] rounded-xl border dark:border-slate-900">
+      <div className=" flex overflow-x-auto gap-x-2 p-3 my-3 rounded-xl border dark:border-slate-900">
         s
       </div>
     </div>
